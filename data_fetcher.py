ec109e8fd4e5377950137cd54d0d5e90b754bb90
Kamal <kamal.laarichi@gmail.com>
2024-06-26 15:03:10 +0200

    using dotenv and gitignore to hide API Key

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,9 +1,13 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def fetch_animal_data(animal_name):
     """Fetches animal data from the Ninja API"""
-    API_KEY = "gTbcY2BwGi1Gj/oarE+aWg==FWIsZ8CbNm9KwGQN"
+    API_KEY = os.getenv("API_KEY")
     BASE_URL = f"https://api.api-ninjas.com/v1/animals?name={animal_name}"
     headers = {'X-Api-Key': API_KEY}
 
