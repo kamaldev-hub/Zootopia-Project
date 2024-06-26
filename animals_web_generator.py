44c4e2caf39f9d7d1d804c74739a117a35242261
Kamal <kamal.laarichi@gmail.com>
2024-06-26 14:47:29 +0200

    new project architecture, fetching data in a separate file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,15 +1,4 @@
-import requests
-
-
-def fetch_animal_data(animal_name):
-    """Fetches animal data from the Ninja API"""
-    API_KEY = "gTbcY2BwGi1Gj/oarE+aWg==FWIsZ8CbNm9KwGQN"
-    BASE_URL = f"https://api.api-ninjas.com/v1/animals?name={animal_name}"
-    headers = {'X-Api-Key': API_KEY}
-
-    response = requests.get(BASE_URL, headers=headers)
-    animals_data = response.json()
-    return animals_data
+from data_fetcher import fetch_animal_data
 
 
 def generate_animal_info(animals_data, animal_name):
